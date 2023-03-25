496de2b825cb9ce344ed3c5e3ba8f68d22b1cfde
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-03-25 17:03:10 +0300

    added db

--- hello.py
+++ hello.py
@@ -2,18 +2,82 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 # create a Flask instance
 app = Flask(__name__)
+app.app_context().push()
+
+"""
+def create_app():
+    app = Flask(__name__)
+
+    with app.app_context():
+        init_db()
+
+    return app
+"""
+
+# create a database instance
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# initialize the db
+db = SQLAlchemy(app)
+
+# create a secret_key
 app.config['SECRET_KEY'] = "my_secret_key"
 
-# create a form class
-class NameForm(FlaskForm):
-    name = StringField("What's your name", validators=[DataRequired()])
+# create model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    # create string representation
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+# create a user-form class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+# create a name-form class
+class NameForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 # create a route decorator
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    # validate form
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User Added Successfully!")
+    our_users = Users.query.order_by(Users.created_at)
+
+    return render_template('add_user.html', form=form, name=name, our_users=our_users)
+
+
 @app.route('/')
 def index():
     first_name = "John"
