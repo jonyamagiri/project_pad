8d92205bf54459faf60eceb3247c6b9f3dd2f720
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-03-25 14:42:55 +0300

    added wtf-forms

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my_secret_key"
+
+# create a form class
+class NameForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 # create a route decorator
 @app.route('/')
@@ -29,6 +38,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+# create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    
+    return render_template('name.html', name=name,form=form)
 
 
 
